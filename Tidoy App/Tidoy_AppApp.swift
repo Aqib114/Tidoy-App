348a93466b26bc0be589c9b98150653943673979
Muhammad Aqib Mehmood <muhammadaqibmehmood73@gmail.com>
2025-04-18 11:57:49 +0500

    first commit

---
+++ Tidoy App/Tidoy_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tidoy_AppApp.swift
+//  Tidoy App
+//
+//  Created by Mapple.pk on 18/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct Tidoy_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
