d33ce51bf0b35305c353493db50b9b6ba9d6b464
Muhammad Aqib Mehmood <muhammadaqibmehmood73@gmail.com>
2025-05-08 10:55:44 +0500

    on boarding screens completed

--- Tidoy App/Views/OnBoarding/OnBoardingScreen.swift
+++ Tidoy App/Views/OnBoarding/OnBoardingScreen.swift
@@ -6,31 +6,124 @@
 //
 
 import SwiftUI
-
+struct OnBoardingDataSourceModel : Hashable{
+    var image: String
+    var title: String
+    var description: String
+}
 
 struct OnBoardingScreen: View {
     @Binding  var isDisabled : Bool
+    @State private var index = 0
+    let screenHeight = UIScreen.main.bounds.height
+    let onBoardingDataSource: [OnBoardingDataSourceModel] =
+    [
+        OnBoardingDataSourceModel(
+            image: "onBoardingImg1",
+            title: "Gateway to Your Adventure",
+            description: "Enjoy various housing options, from budget to luxury, in Tidoy."
+        ),
+        OnBoardingDataSourceModel(
+            image: "onBoardingImg2",
+            title: "Discover the Wonders of the World: Let's Explore!",
+            description: "Book a stay wherever you are, whenever you want."
+        ),
+        OnBoardingDataSourceModel(
+            image: "onBoardingImg3",
+            title: "The Right Solution for Your Holiday Accommodation",
+            description: "A stress-free Holiday? Trust your Holiday accommodation to Tidoy!"
+        )
+    ]
+    
     var body: some View {
-        ZStack(){
-            VStack(alignment: .leading){
-                Image("onBoardingImg1")
-                    .padding(.vertical, 28)
-                Text("Gateway to Your \nAdventure")
-                    .font(.heading4)
-                    .foregroundStyle(.text100)
-                    .padding(.horizontal, 16)
-                Text("Enjoy various housing options, from\nbudget to luxury, in Tidoy.")
-                    .padding(.horizontal, 16)
-                    .foregroundStyle(.text60)
-                HStack {
-                    PrimaryButton(title: "Skip", disabled: $isDisabled, action: {})
-                        .frame(width: 63)
-                    PrimaryButton(title: "Next", disabled: $isDisabled, action: {})
+        VStack(){
+            tabView
+            pageView
+            buttonsView
+        }
+    }
+    
+    private var tabView: some View {
+        TabView(selection: $index){
+            ForEach(0..<onBoardingDataSource.count, id: \.self){
+                index in
+                let model = onBoardingDataSource[index]
+                VStack(alignment: .leading){
+                    Image(model.image)
+                        .rotationEffect(.degrees(index == self.index ? 360 : 0))
+                    Text(model.title)
+                        .font(.heading4)
+                        .foregroundStyle(.text100)
+                        .padding(.horizontal, 16)
+                    Text(model.description)
+                        .padding(.horizontal, 16)
+                        .foregroundStyle(.text60)
                 }
-                .padding(.horizontal, 32)
-                Spacer()
+                .transition(.slide)
             }
         }
+        .tag(index)
+        .tabViewStyle(.page(indexDisplayMode: .never))
+        .padding(.horizontal, 16)
+        .frame(height: screenHeight * 0.67)
+    }
+    
+    private var pageView: some View {
+        HStack{
+            ForEach(onBoardingDataSource.indices, id: \.self) { index in
+                if self.index == index  {
+                    RoundedRectangle(cornerRadius: 5)
+                        .foregroundColor(.primaryMain)
+                        .frame(width: 26, height: 8)
+                        .transition(.scale)
+                }
+                else {
+                    Circle()
+                        .foregroundStyle(.background30)
+                        .frame(height: 8)
+                }
+            }
+        }
+        .frame(height: 20)
+    }
+    
+    private var buttonsView: some View {
+        HStack (spacing: 12)
+        {
+            if index < onBoardingDataSource.count - 1
+            {
+                SecondaryButton(
+                    title: "Skip",
+                    action: {
+                        withAnimation{
+                            index = onBoardingDataSource.count - 1
+                        }
+                    })
+                .frame(width: 63)
+            }
+            PrimaryButton(
+                title: index < onBoardingDataSource.count - 1
+                ?"Next"
+                :"Get Started",
+                rightIcon: index<onBoardingDataSource.count - 1
+                ? Image(systemName :"arrow.forward")
+                : nil,
+                disabled: $isDisabled,
+                action:{
+                    withAnimation{
+                        if index < onBoardingDataSource.count - 1 {
+                            index += 1
+                        }
+                        else
+                        {
+                            
+                        }
+                    }
+                }
+                
+            )
+        }
+        .padding(.horizontal, 32)
     }
 }
 
