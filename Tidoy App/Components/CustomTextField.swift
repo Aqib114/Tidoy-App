be35c7ed4c954a73f38db74ad1150add90197f3b
Muhammad Aqib Mehmood <muhammadaqibmehmood73@gmail.com>
2025-05-09 15:12:29 +0500

    added custom textfields

--- Tidoy App/Components/CustomTextField.swift
+++ Tidoy App/Components/CustomTextField.swift
@@ -8,11 +8,113 @@
 import SwiftUI
 
 struct CustomTextField: View {
+    @Binding var state: TextFieldState
+    @Binding var text : String
+    var title: String
+    var leftIcon : Image
+    var rightIcon : Image
+    var hint: String?
+    var placeholder: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 6.0){
+            Spacer()
+            Text(title)
+                .font(.bodySMedium)
+                .foregroundStyle(state.labelColor)
+            RoundedRectangle(cornerRadius: .cornerRadiusM)
+                .fill(state.backgroundColor)
+                .stroke(state.borderColor, lineWidth: 1)
+                .frame(height: 50)
+                .overlay(){
+                    HStack{
+                        leftIcon
+                            .foregroundStyle(state.iconColor)
+                        TextField("", text: $text)
+                            .disabled(state == .disable ? true : false)
+                            .placeholder(when: text.isEmpty) {
+                                    Text("Placeholder recreated").foregroundColor(.gray)
+                            }
+                            .foregroundStyle(state.labelColor)
+                            .font(.bodySMedium)
+                        
+                        rightIcon
+                            .foregroundStyle(state.iconColor)
+                    }
+                    .padding(.horizontal, .padding12)
+                }
+            if let hint = hint {
+                HStack {
+                    if state == .error {
+                        Image(systemName: "exclamationmark.circle.fill")
+                            .foregroundStyle(state.iconColor)
+                    }
+                    Text(hint)
+                        .font(.bodyXSRegular)
+                        .foregroundStyle(state.hintColor)
+                }
+            }
+            Spacer()
+        }
+        .padding(.horizontal, .padding16)
     }
 }
 
 #Preview {
-    CustomTextField()
+    PreviewWrapper()
+}
+
+private struct PreviewWrapper: View {
+    @State private var text = ""
+
+    var body: some View {
+        VStack(spacing: 10) {
+            CustomTextField(
+                state: .constant(.normal),
+                text: $text,
+                title: "Label",
+                leftIcon: Image(systemName: "plus.circle"),
+                rightIcon: Image(systemName: "plus.circle"),
+                hint: "This is an normal message",
+                placeholder: "Enter text here"
+            )
+            CustomTextField(
+                state: .constant(.focus),
+                text: $text,
+                title: "Label",
+                leftIcon: Image(systemName: "plus.circle"),
+                rightIcon: Image(systemName: "plus.circle"),
+                hint: "This is an focus message",
+                placeholder: "Enter text here"
+            )
+            CustomTextField(
+                state: .constant(.filled),
+                text: $text,
+                title: "Label",
+                leftIcon: Image(systemName: "plus.circle"),
+                rightIcon: Image(systemName: "plus.circle"),
+                hint: "This is an filled message",
+                placeholder: "Enter text here"
+            )
+            CustomTextField(
+                state: .constant(.disable),
+                text: $text,
+                title: "Label",
+                leftIcon: Image(systemName: "plus.circle"),
+                rightIcon: Image(systemName: "plus.circle"),
+                hint: "This is an disable message",
+                placeholder: "Enter text here"
+            )
+            CustomTextField(
+                state: .constant(.error),
+                text: $text,
+                title: "Label",
+                leftIcon: Image(systemName: "plus.circle"),
+                rightIcon: Image(systemName: "plus.circle"),
+                hint: "This is an error message",
+                placeholder: "Enter text here"
+            )
+        }
+        .padding()
+    }
 }
--- Tidoy App/Constants/Enums/Enum+TextFieldStates.swift
+++ Tidoy App/Constants/Enums/Enum+TextFieldStates.swift
@@ -6,3 +6,74 @@
 //
 
 import Foundation
+import SwiftUI
+
+enum TextFieldState {
+    case normal, focus, filled, disable, error
+}
+
+extension TextFieldState {
+    
+    var borderColor: Color {
+        switch self {
+        case .normal, .filled, .disable:
+            return .stroke40
+        case .focus:
+            return .strokeFocus
+        case .error:
+            return .strokeDanger
+        }
+    }
+    var backgroundColor: Color {
+        switch self {
+        case .normal, .focus, .filled:
+            return .background10
+        case .disable:
+            return .background20
+        case .error:
+            return .backgroundDanger
+        }
+    }
+    
+    var iconColor: Color {
+        switch self {
+        case .normal, .disable:
+            return .icon60
+        case .focus, .filled:
+            return .icon100
+        case .error:
+            return .iconDanger
+        }
+    }
+    var hintColor: Color {
+        switch self {
+        case .normal, .focus, .filled:
+            return .text100
+        case  .disable:
+            return .text60
+        case .error:
+            return .textDanger
+        }
+    }
+    var placeholderColor: Color {
+        switch self {
+        case .normal, .disable:
+            return .text60
+        case .focus, .filled:
+            return .text100
+        case .error:
+            return .textDanger
+        }
+    }
+    var labelColor: Color {
+        switch self {
+        case .normal, .disable, .focus, .filled:
+            return .text100
+        case .error:
+            return .textDanger
+        }
+    }
+    
+}
+
+
